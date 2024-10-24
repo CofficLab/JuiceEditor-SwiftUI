4df92ae79824c564e6ba8fa9cb3af4d60e3ef234
Nookery <nooks@qq.com>
2024-10-24 17:46:08 +0800

    📦 Package Update:  Update package dependencies

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/CofficLab/MagicKit.git", branch: "main")
+        .package(url: "https://github.com/CofficLab/MagicKit.git", from: "1.0.0")
     ],
     targets: [
         .target(
